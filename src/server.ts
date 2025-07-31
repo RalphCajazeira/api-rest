85de3e676945d1e2ea5cdab26bbfe5cbc117f2c3
Ralph Ramos <ralphmtk@gmail.com>
2025-07-31 14:07:16 -0300

    Tratando requisição

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes";
+
+import { AppError } from "./utils/AppError";
 
 const PORT: number = 3333;
 
@@ -7,6 +9,19 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+/**
+ * 400 (Bad Request): Erro do cliente.
+ * 500 (Internal Server Error): Erro interno do servidor
+ */
+
+app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  response.status(500).json({ message: "Erro Genérico" });
+});
 
 app.listen(PORT, () =>
   console.log(`Server is running at http://localhost:${PORT}/`)
