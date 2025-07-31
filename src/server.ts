3ff1c816c374207edc223ffdfb6fca42f4882aca
Ralph Ramos <ralphmtk@gmail.com>
2025-07-31 10:11:43 -0300

    Controladores separados

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,12 @@
 import express from "express";
-import { myMiddleware } from "./middlewares/my-middleware";
+import { routes } from "./routes";
 
 const PORT: number = 3333;
 
 const app = express();
 app.use(express.json());
 
-// Middleware Global
-// app.use(myMiddleware);
-
-app.get("/products", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.json(`Pagina ${page ?? 1} de ${limit ?? 10}`);
-});
-
-// Middleware local em uma rota especifica.
-app.post("/products", myMiddleware, (request, response) => {
-  const { name, price } = request.body;
-
-  response.status(201).json({ name, price });
-});
+app.use(routes);
 
 app.listen(PORT, () =>
   console.log(`Server is running at http://localhost:${PORT}/`)
